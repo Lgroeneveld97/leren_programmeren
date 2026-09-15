b6a0e3ab4e2a238eaee0087fb37c43a4c3418453
Lgroeneveld97 <lucasgroeneveld97@gmail.com>
2026-09-15 09:59:49 +0200

    elif statement

---
+++ module_1/deel_2/meten_is_weten.py
@@ -0,0 +1,7 @@
+nr1 = 13
+nr2 = 10
+
+if nr1 == nr2:
+    print(f"Beide getallen zijn even groot (nr1: {nr1}, nr2: {nr2})")
+elif nr1 >= nr2:
+    print(f"nr1 is groter dan nr2 (nr1: {nr1}, nr2: {nr2})")
